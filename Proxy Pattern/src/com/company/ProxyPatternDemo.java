b739c7c71ca9bd400752dea713ee3f0524eb9409
root113 <vsametustun@gmail.com>
2019-12-12 23:34:30 +0300

    4 new patterns

---
+++ Proxy Pattern/src/com/company/ProxyPatternDemo.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class ProxyPatternDemo {
+
+    public static void main(String[] args) {
+        Image image = new ProxyImage("test_10mb.jpg");
+
+        //Image will be loaded from disk
+        image.display();
+        System.out.println("");
+
+        //Image will not be loaded from disk
+        image.display();
+    }
+}
